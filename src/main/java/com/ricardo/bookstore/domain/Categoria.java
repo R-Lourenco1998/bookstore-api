8429e4430954951ab61e5aa91f1956b08b619ffa
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2022-09-24 18:08:11 -0300

    finalizando dominio com relacionamento

--- src/main/java/com/ricardo/bookstore/domain/Categoria.java
+++ src/main/java/com/ricardo/bookstore/domain/Categoria.java
@@ -5,18 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Categoria {
+@Entity()
+public class Categoria implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria(Integer id, String nome, String descricao) {
--- src/main/java/com/ricardo/bookstore/domain/Livro.java
+++ src/main/java/com/ricardo/bookstore/domain/Livro.java
@@ -2,18 +2,27 @@ package com.ricardo.bookstore.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
 public class Livro {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
 }
