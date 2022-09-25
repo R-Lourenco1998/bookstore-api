b15536f195458df82bfc97c21a79cff932323a1c
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2022-09-25 12:47:16 -0300

    Adicionando dados no banco de dados com Command Line Runner

--- src/main/java/com/ricardo/bookstore/BookstoreApplication.java
+++ src/main/java/com/ricardo/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.ricardo.bookstore;
 
+import com.ricardo.bookstore.domain.Categoria;
+import com.ricardo.bookstore.domain.Livro;
+import com.ricardo.bookstore.repositories.CategoriaRepository;
+import com.ricardo.bookstore.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+
+		cat1.getLivros().addAll(List.of(l1));
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }
