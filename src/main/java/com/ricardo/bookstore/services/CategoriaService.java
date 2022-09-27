9d200113fc93ae730ac09c5cf5152897361efc5a
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2022-09-26 21:02:38 -0300

    criando excessões personalizadas

--- src/main/java/com/ricardo/bookstore/services/CategoriaService.java
+++ src/main/java/com/ricardo/bookstore/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.ricardo.bookstore.services;
 
 import com.ricardo.bookstore.domain.Categoria;
 import com.ricardo.bookstore.repositories.CategoriaRepository;
+import com.ricardo.bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
