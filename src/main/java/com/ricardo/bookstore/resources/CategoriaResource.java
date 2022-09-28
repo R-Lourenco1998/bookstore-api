6c417900c3c7f918a766fd265cc22785030bc99f
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2022-09-27 21:06:41 -0300

    post para criar categoria

--- src/main/java/com/ricardo/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/ricardo/bookstore/resources/CategoriaResource.java
@@ -5,11 +5,10 @@ import com.ricardo.bookstore.dtos.CategoriaDTO;
 import com.ricardo.bookstore.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,10 +27,15 @@ public class CategoriaResource {
 
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> findAll(){
-
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDto = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+        obj = categoriaService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
